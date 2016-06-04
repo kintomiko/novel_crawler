999d526fdd987611ec97e238f394bf84f6055fea
ylin <yuanlin1110@gmail.com>
2016-06-05 01:58:00 +0800

    all domain filter

--- master.py
+++ master.py
@@ -4,19 +4,13 @@ import config
 from pybloom import ScalableBloomFilter
 sbf = ScalableBloomFilter(mode=ScalableBloomFilter.SMALL_SET_GROWTH)
 
-logger = logging.getLogger('Slave')
-logger.setLevel(logging.INFO)
-
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.INFO)
-
-logger.addHandler(ch)
+logger = Logger.getStdOutDebugLogger('master')
 
 redis_conn = redis.StrictRedis(config.REDIS_HOST, config.REDIS_PORT)
 
 while(True):
   url = redis_conn.blpop(config.RawQueue)[1]
 
-  if not url in sbf:
+  if (not url in sbf) and (url[0:url.find('/', 7)].find('2epub') != -1):
     sbf.add(url)
     redis_conn.rpush(config.PendingCrawlingQueue, url)
